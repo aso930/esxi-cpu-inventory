dec6ab0a07661cb845c2af82916643a1b910a0c1
aso930 <alexandru.asofroniei@outlook.com>
2017-08-09 15:32:14 +0300

    Initial commit

---
+++ esxi-cpu-inventory.ps1
@@ -0,0 +1,31 @@
+﻿
+param(
+    [Parameter(Mandatory=$true)][string]$server
+)
+Connect-VIServer -Server $server -Protocol https -WarningAction 0 | out-null;
+$cpuTotalAssigned=0;
+$cpuTotalAssignedPowered=0;
+$cpuTotalAvailable=0;
+foreach($cpu in (Get-VM | Select PowerState, NumCPU))
+{
+    $cpuTotalAssigned += [convert]::ToInt32($cpu.NumCpu);
+    if($cpu.PowerState -eq "PoweredOn")
+    {
+        $cpuTotalAssignedPowered += [convert]::ToInt32($cpu.NumCpu);
+    }
+
+}
+$hostDetail=(get-vmhost | select NumCPU, HyperthreadingActive);
+if($hostDetail.HyperthreadingActive)
+{
+    $cpuTotalAvailable = $hostDetail.NumCpu * 2; 
+
+}
+else
+{
+    $cpuTotalAvailable = $hostDetail.NumCpu;
+}
+
+Write-Host "Host $server has $cpuTotalAssignedPowered vCPU's assigned to running VM's, out of $cpuTotalAvailable cores available. The number of total vCPU's assigned is $cpuTotalAssigned."
+
+Disconnect-VIServer -Confirm:$false
